6cfacc2858e56c18b01dbf646ea5d1135126c3a3
ElVectores <victor.orlando.acero@estudiantat.upc.edu>
2026-04-25 16:06:49 +0200

    Update main.py
    He cambiado los bands por los que realmente usaremos y creado una nueva funcion para el indice de tres bandas

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ def process_image(band_paths):
 
     # Carregar les bandes necessàries
     try:
-        b3 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B03.jp2"), masked=True)
-        b4 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B04.jp2"), masked=True)
-        b5 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B05.jp2"), masked=True)
-        b8 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B08.jp2"), masked=True)
+        b7 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B07.jp2"), masked=True)
+        b14 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B14.jp2"), masked=True)
+        b15 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B15.jp2"), masked=True)
+        b18 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B18.jp2"), masked=True)
+        b28 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B28.jp2"), masked=True)
+        b21 = rioxarray.open_rasterio(os.path.join(RAW_BANDS_DIR, f"{band_paths}_B21.jp2"), masked=True)
+
     
     except FileNotFoundError as e:
         print(f"Error: No s'han trobat les bandes per a: {band_paths}. {e}")
@@ -31,13 +34,22 @@ def process_image(band_paths):
         # Evitar divisions per zero
         denominator = np.where(denominator != 0, np.nan)
         return (banda_a - banda_b) / denominator
+    def normalized2_diff(banda_a, banda_b, band_c):
+        denominator = banda_a
+        denominator2 = banda_b
+        # Evitar divisions per zero
+        denominator = np.where(denominator != 0, np.nan)
+        denominator2 = np.where(denominator2 != 0, np.nan)
+        return (denominator - denominator2) * band_c
     
 
     # Calcular indexs
-    ndwi = normalized_diff(b3, b8)  # Mascara d'aigua
-    ndvi = normalized_diff(b8, b4)   # Vegetació/Algas superficals
-    ndti = normalized_diff(b4, b3)  # Turbidez 
-    ndci = normalized_diff(b5, b4)  # Cloròfila
+    ndwi = normalized_diff(b7, b28)  # Mascara d'aigua
+    ndvi = normalized_diff(b28, b15)   # Vegetació/Algas superficals
+    ndti = normalized_diff(b14, b7)  # Turbidez 
+    ndci = normalized_diff(b18, b15)  # Cloròfila
+    tbm = normalized2_diff(b15, b18, b21)  # Indice de clorofila tocho
+
 
     # Generacio del nostre index d'alerta (alerta eutrofització)
     alerta_eutroficacion = (ndwi > UMBRAL_AGUA) & (ndci > UMBRAL_EUTROF)    #calcul que es pot millorar, però és un punt de partida
